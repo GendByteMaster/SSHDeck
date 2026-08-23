5bacdf07fbcf4b9b00f135099dc17f1979c1fcfa
Gend <GendByteMaster@gmail.com>
2026-08-23 17:00:00 +0300

    feat: add server model

---
+++ src/server.rs
@@ -0,0 +1,10 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Server {
+    pub alias: String,
+}
+
+impl Server {
+    pub fn new(alias: impl Into<String>) -> Self {
+        Self { alias: alias.into() }
+    }
+}
