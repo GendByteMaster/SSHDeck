324460c7e080741c5622f74f10cd68ab5c9f160e
Gend <GendByteMaster@gmail.com>
2026-08-23 19:05:15 +0300

    feat: enable macOS-inspired visual layer

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "@xterm/xterm/css/xterm.css";
 import "./styles.css";
 import "./sessionLifecycle.css";
+import "./apple.css";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
