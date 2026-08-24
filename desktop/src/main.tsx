7ee3c65841b59e7b3f233223901234e50aeee504
Gend <GendByteMaster@gmail.com>
2026-08-24 11:31:10 +0300

    feat: mount native application menu

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -7,16 +7,18 @@ import "./performance.css";
 import "./globals.css";
 import { App } from "./App";
 import { CommandPalette } from "./CommandPalette";
-import { CommandProvider } from "./commands/CommandService";
-import { KeybindingService } from "./commands/KeybindingService";
 import { DesktopShortcuts } from "./DesktopShortcuts";
 import { WorkbenchChrome } from "./WorkbenchChrome";
 import { WorkbenchProvider } from "./WorkbenchContext";
+import { CommandProvider } from "./commands/CommandService";
+import { KeybindingService } from "./commands/KeybindingService";
+import { NativeMenu } from "./commands/NativeMenu";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <WorkbenchProvider>
     <CommandProvider>
       <KeybindingService />
+      <NativeMenu />
       <App />
       <WorkbenchChrome />
       <CommandPalette />
