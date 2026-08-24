51db215e29ea7bf26291a194a6c03e208342b9ea
Gend <GendByteMaster@gmail.com>
2026-08-24 11:01:38 +0300

    refactor: remove imperative keyboard bootstrap

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -4,21 +4,18 @@ import "./design-system.css";
 import "./styles.css";
 import "./sessionLifecycle.css";
 import "./performance.css";
-import "./keyboard.css";
 import "./globals.css";
 import { App } from "./App";
 import { CommandPalette } from "./CommandPalette";
+import { DesktopShortcuts } from "./DesktopShortcuts";
 import { WorkbenchChrome } from "./WorkbenchChrome";
 import { WorkbenchProvider } from "./WorkbenchContext";
-import { installKeyboardNavigation } from "./keyboard";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <WorkbenchProvider>
     <App />
     <WorkbenchChrome />
     <CommandPalette />
+    <DesktopShortcuts />
   </WorkbenchProvider>,
 );
-
-const uninstallKeyboardNavigation = installKeyboardNavigation();
-window.addEventListener("beforeunload", uninstallKeyboardNavigation, { once: true });
