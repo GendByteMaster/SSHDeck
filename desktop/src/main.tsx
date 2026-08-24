5a6e9704544d16c179a9980e7fc02feb7185cb1e
Gend <GendByteMaster@gmail.com>
2026-08-24 09:27:33 +0300

    fix: load responsive workbench corrections last

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -14,6 +14,7 @@ import "./performance.css";
 import "./keyboard.css";
 import "./workbench-v4.css";
 import "./workbench-chrome.css";
+import "./workbench-responsive-v4.css";
 import { App } from "./App";
 import { WorkbenchChrome } from "./WorkbenchChrome";
 import { installKeyboardNavigation } from "./keyboard";
